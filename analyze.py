715d3c610f23570c187a93ca47c5114bc328598e
cssummer16 <cssummer16@c35.clinic.cs.hmc.edu>
2016-06-27 09:41:48 -0700

    Added error metrics

--- analyze.py
+++ analyze.py
@@ -262,6 +262,7 @@ class analyzer(object):
         red_ellipse = cv2.fitEllipse(red_c)
         red_center = (int(red_ellipse[0][0]), int(red_ellipse[0][1]))
 
+
         return green_center, red_center
 
     def writeCoord(self, filename, mode):
@@ -279,8 +280,46 @@ class analyzer(object):
             file.write(str(index[0]) + '\n')
             file.write(str(index[1]) + '\n')
 
+    def readBestGuess(self, filename):
+        '''this function reads the list of best guesses of the robot's position at every position'''
+        file = open(filename, 'r')
+        content = file.read().split('\n')[:-1]
+        content = list(map(int, content))
+        bestGuesses = [[content[x], content[x+1]] for x in range(len(content) - 1 ) [::2]    ]
+        return bestGuesses
 
+    def readCoord(self, filename):
+        file = open(filename, 'r')
+        content = file.read().split('\n')[:-1]
+        coordinates = [list(map(int, coord.split(','))) for coord in content]
+        return coordinates
 
+    def errorMetric(self):
+        ''' this function calculate the differences between best guesses and the true values'''
+        bestGuess = self.readBestGuess('bestGuess.txt')
+        # best Guess contains a list of lists. First elements is the index of the best cirlce, second element is the index
+        # of the best angle
+        coordinates = self.readCoord('coord.txt')
+        # coordinates is the "real" position of the robot as analyzed by the image matching algorithm
+        # the first two points of coordinates is the position of the robot, the second set of points
+        # are direction of the angle 
+        angleError = 0
+        for i in range(len(bestGuess)):
+            bestGuessAngle = bestGuess[i][1] * 15
+            robotPos = coordinates[i][:2]
+            robotDir = coordinates[i][2:]
+            angle = math.atan2(robotDir[1] - robotPos[1], robotDir[0] - robotPos[0])
+            angle *= 180./math.pi
+            angle += 90
+            if angle <= 0:
+                angle += 360
+            # print (angle)
+            # print ("best: " + str(bestGuessAngle))
+            if bestGuessAngle == 0 and angle > 300:
+                bestGuessAngle = 360
+            elif bestGuessAngle == 360 and angle < 60:
+                bestGuessAngle = 0
+            angleError += (bestGuessAngle - angle)**2
 
-
+        return math.sqrt(angleError)
             
